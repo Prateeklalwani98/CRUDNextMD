7d0c3e51900ce012365b18cd13c5c0243ea5c12f
Prateek Lalwani <prateeklalwani98@gmail.com>
2024-06-01 18:25:05 +0530

    next conf

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+  env: {
+    MONGODB_URI:
+      "mongodb+srv://prateeklalwani005:Du45WfmqxTT3hx8g@crudnextmd.kuqo3my.mongodb.net/?retryWrites=true&w=majority&appName=crudNextMD",
+  },
+};
 export default nextConfig;
